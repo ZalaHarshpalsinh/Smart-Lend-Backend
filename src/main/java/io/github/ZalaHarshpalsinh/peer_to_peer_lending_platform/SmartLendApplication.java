a30dfd481ed9e5733893c61693e6d45c868b9fb4
Harshpalsinh <zalaharshpalsinh511@gmail.com>
2025-05-03 23:18:17 +0530

    Initial commit

---
+++ src/main/java/io/github/ZalaHarshpalsinh/peer_to_peer_lending_platform/SmartLendApplication.java
@@ -0,0 +1,13 @@
+package io.github.ZalaHarshpalsinh.peer_to_peer_lending_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ZalaHarshpalsinh/peer_to_peer_lending_platform/SmartLendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ZalaHarshpalsinh.peer_to_peer_lending_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
